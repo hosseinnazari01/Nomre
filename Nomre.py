7f8eeb38fa2df9d4d1b9be47bd91e0ec9a9d6309
Hossein Nazari <hossein.nazari.computer01@gmail.com>
2026-04-26 13:49:39 +0330

    Create Nomre.py
    Create a Python Project Nomre

---
+++ Nomre.py
@@ -0,0 +1,11 @@
+Num=int(input("Enter a Number: "))
+if Num < 10 and Num >=0:
+    print("Mardood!")
+elif Num >=10 and Num <=15:
+    print("Motevaset!")
+elif Num >=15 and Num <=20:
+    print("Aalli!")
+elif Num > 20:
+    print("More Than 20!")
+else:
+    print("Error!")
